5cb520539d794feec8cdb3d6fed57ed8bf46c85d
otrvue <otrvue@gmail.com>
2026-06-03 19:10:22 +0700

    chore: remove ignored files from repository

--- scripts/add_reset_password_columns.sql
+++
@@ -1,7 +0,0 @@
--- Add columns for password reset functionality
-ALTER TABLE users
-ADD COLUMN passwordResetToken VARCHAR(255) DEFAULT NULL,
-ADD COLUMN passwordResetExpiresAt DATETIME DEFAULT NULL;
-
--- Add index for faster token lookup
-CREATE INDEX idx_password_reset_token ON users (passwordResetToken);
